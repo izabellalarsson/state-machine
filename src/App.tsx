ae69191bb9cd50c8d3264bc1de8220b5e9013331
Izabella Larsson Polestar <izabella.larsson@polestar.com>
2023-09-28 12:02:54 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import './App.css'
+import { useReducer } from "react";
+
+type State = "lit" | "unlit" | "broken"
+
+type Event = "BREAK" | "TOGGLE";
+
+type NextStateGraph = {
+  [key in State]: {
+    [key in Event]?: State;
+  };
+};
+
+const reducer = (state: State, event: Event) => {
+  const nextState = NEXT_STATE_GRAPH[state][event] as State | undefined;
+  return nextState !== undefined ? nextState : state;
+};
+
+const NON_RESPONSIVE_EVENTS: { [key in Event]: undefined } = {
+  BREAK: undefined,
+  TOGGLE: undefined,
+};
+
+const NEXT_STATE_GRAPH: NextStateGraph = {
+  lit: {
+    ...NON_RESPONSIVE_EVENTS,
+    BREAK: "broken",
+    TOGGLE: "unlit",
+  },
+  unlit: {
+    ...NON_RESPONSIVE_EVENTS,
+    BREAK: "broken",
+    TOGGLE: "lit",
+  },
+  broken: {
+    ...NON_RESPONSIVE_EVENTS,
+  },
+};
+
+function LightBulb() {
+  const [state, send] = useReducer(reducer, "unlit");
+
+  return (
+    <div>
+      State: {state}
+      <button type="button" onClick={() => send("TOGGLE")}>
+        Toggle
+      </button>
+      <button type="button" onClick={() => send("BREAK")}>
+        Break
+      </button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <LightBulb/>
+    </>
+  )
+}
+
+export default App
